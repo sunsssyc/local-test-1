87293e90aa3027efecb15217daee97c13d6a00c9
sunsssyc <sun520520yc@gmail.com>
2023-05-07 20:52:26 +0800

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,27 +1,18 @@
 # 插入排序
 
 def inserttion(arr):
-    for i in arr[1:]:
+    for i in range(len(arr[1:])):
         curr = arr[i]
-        j = i-1
-        while j>=0 and arr[j]>curr:
-              arr[j+1] = arr[j]
-	      j -= 1
-        arr[j+1] = curr
+        j = i - 1
+        while j >= 0 and arr[j] > curr:
+            arr[j + 1] = arr[j]
+            j -= 1
+    arr[j + 1] = curr
+
+
     return arr
 
-
 if __name__ == '__main__':
-   arr = [1,6,7,90,99]
-   arr1 = inserttion(arr)
-   print(arr1)							
-
-
-
-
-
-
-
-
-
-
+    arr = [1, 6, 7, 90, 99]
+    arr1 = inserttion(arr)
+    print(arr1)
